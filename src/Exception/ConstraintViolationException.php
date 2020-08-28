665a75efd69fcf1ee4998db5c0c05d748c7cfb12
danigore <danigoredev@gmail.com>
2020-08-28 22:47:55 +0200

    Expection class for constraint violations

---
+++ src/Exception/ConstraintViolationException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Exception;
+
+/**
+ * Class ConstraintViolationException
+ * @package App\Exception
+ */
+class ConstraintViolationException extends \Exception
+{
+    // ...
+}
