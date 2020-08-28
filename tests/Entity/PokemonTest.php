d11b9fd710b528b634e0130dc6a4f011b7f7f16a
danigore <danigoredev@gmail.com>
2020-08-28 23:20:07 +0200

    TDD: Test the constraint violations of a Pokemon

---
+++ tests/Entity/PokemonTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Pokemon;
+use App\Tests\AbstractEntityTest;
+
+/**
+ * Class PokemonTest
+ * @package App\Tests\Entity
+ */
+class PokemonTest extends AbstractEntityTest
+{
+    /**
+     * @return void
+     */
+    public function testConstraintViolations(): void
+    {
+        $pokemon = new Pokemon();
+        $this->assertSame('normal', $pokemon->getType());
+        $this->expectedConstraintViolationExceptionOnPersist($pokemon, 'Every pokemon has a name!');
+
+        $pokemon->setName('Pikachu');
+        $this->entityIsValidSoPersistIsPossible($pokemon);
+
+        $pokemon->setType('whatever');
+        $this->expectedConstraintViolationExceptionOnPersist($pokemon, 'whatever is not a valid type of Pokemon!');
+
+        $pokemon->setType('electric');
+        $this->entityIsValidSoPersistIsPossible($pokemon);
+    }
+}
